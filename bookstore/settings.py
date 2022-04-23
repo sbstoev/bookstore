789b9f7242ad12dfc2206215da500edfc61f1da8
Stoytcho Stoev <stoytcho.b.stoev@gmail.com>
2022-04-23 17:28:30 +0300

    Fixed media files

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -165,7 +165,7 @@ MEDIA_ROOT = BASE_DIR / 'mediafiles'
 MEDIA_URL = '/media/'
 '''
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 # Default primary key field type
