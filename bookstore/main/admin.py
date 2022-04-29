123265a774cb2f5e47c290d3d4bf180844977c5b
Stoytcho Stoev <stoytcho.b.stoev@gmail.com>
2022-04-29 11:55:12 +0300

    Added admins

--- bookstore/main/admin.py
+++ bookstore/main/admin.py
@@ -1,8 +1,18 @@
 from django.contrib import admin
 
-from bookstore.main.models import Book
+from bookstore.main.models import Book, Author, Event
 
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author')
+
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name')
+
+
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('title', 'city', 'country', 'date')
